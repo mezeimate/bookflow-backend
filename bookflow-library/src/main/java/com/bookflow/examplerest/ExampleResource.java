17faef056fa05900185fdb92770cc0efb9890744
Feeld <mezeimate000@gmail.com>
2023-06-19 00:55:54 +0200

    initial commit #2

--- bookflow-library/src/main/java/com/bookflow/examplerest/ExampleResource.java
+++ bookflow-library/src/main/java/com/bookflow/examplerest/ExampleResource.java
@@ -1,16 +1,16 @@
-package com.bookflow;
+package com.bookflow.examplerest;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/common")
+@Path("/library")
 public class ExampleResource {
-
+    
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces({MediaType.APPLICATION_JSON})
     public String hello() {
-        return "common";
+        return "library";
     }
 }
