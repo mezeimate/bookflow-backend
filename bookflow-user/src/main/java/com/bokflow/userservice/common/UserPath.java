d775597ac91b92d0359bc65b48eca2893d66ac58
Feeld <mezeimate000@gmail.com>
2023-07-17 23:35:46 +0200

    user module creation

---
+++ bookflow-user/src/main/java/com/bokflow/userservice/common/UserPath.java
@@ -0,0 +1,10 @@
+package com.bokflow.userservice.common;
+
+import jakarta.enterprise.inject.Vetoed;
+
+@Vetoed
+public class UserPath {
+
+    public static final String USER_REST_BASE_PATH = "/user";
+
+}
---
+++ bookflow-user/src/main/java/com/bokflow/userservice/rest/UserRest.java
@@ -0,0 +1,14 @@
+package com.bokflow.userservice.rest;
+
+import com.bokflow.userservice.common.UserPath;
+import jakarta.ws.rs.Path;
+
+/**
+ * Rest interface for User
+ *
+ * @author mezeim
+ * @since 0.0.1-SNAPSHOT
+ */
+@Path(UserPath.USER_REST_BASE_PATH)
+public interface UserRest {
+}
